f18e26bf666b45e0cf2602abf2114c100411166f
yeheskielaji <94420820+yeheskielaji@users.noreply.github.com>
2023-06-09 17:35:33 +0700

    index hampir

--- index.php
+++ index.php
@@ -6,20 +6,21 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
-  <title>Toko Medical</title>
+  <title>Percetakaan</title>
   <link rel="stylesheet" href="style/style.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 
 <body>
-  <nav class="navbar fixed-top navbar-expand-lg background-primary" style="height:60px;">
+  <nav class="navbar nav-large fixed-top navbar-expand-lg" style="height:60px; background: white;">
     <div class="container-fluid">
-      <a class="navbar-brand" href="index.php" style="font-weight:bolder; color:#00A445;">
-        <img src="asset/logo putih.png" alt="logo" style="height: 25px; margin-top: -7px; padding-left: 4px;"></a>
+      <a class="navbar-brand" href="index.php" style="font-weight:bolder;">
+        <img src="asset/logo navbar.png" alt="logo" style="height: 30px; padding-left: 4px;"></a>
       <a class="login-min d-flex" href="login.php" style="text-decoration: none;">
-        <button type="button" class="btn btn-outline-light">Login</button>
+        <button type="button" class="btn btn-outline-warning">Login</button>
       </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent" style="font-size:20px;">
@@ -36,37 +37,15 @@
           <li class="nav-item">
             <a class="nav-link text-white " href="riwayat.php">Riwayat</a>
           </li>
-        </ul>
-        <a class="login-max d-flex" href="login.php" style="text-decoration: none;">
-          <button type="button" class="btn btn-outline-light">Login</button>
-        </a>
-      </div>
+        </ul> -->
+    </div>
     </div>
   </nav>
-  <div class="carousel-all container d-flex justify-content-center align-item-center">
-    <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel" style="height:200px; margin: 90px 50px 130px; margin-left: 32px; ">
-      <div class="carousel-inner">
-        <div class="carousel-item active" data-bs-interval="5000">
-          <img src="asset/slide1.png" class="d-block w-90">
-        </div>
-        <div class="carousel-item" data-bs-interval="5000">
-          <img src="asset/slide2.png" class="d-block w-90">
-        </div>
-        <div class="carousel-item">
-          <img src="asset/slide3.png" class="d-block w-90">
-        </div>
-      </div>
-      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev" style="padding-top:60px;">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next" style="padding-top:60px;">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
-    </div>
-  </div>
 
+  <div class="input-group mb-3 mx-auto" style="padding-top: 80px; width:380px;">
+    <input type="text" class="form-control" placeholder="search" aria-label="Recipient's username" aria-describedby="button-addon2">
+    <button class="btn btn-outline-secondary" type="button" id="button-addon2"><i class="bi bi-search"></i></button>
+  </div>
 
   <div id="produk">
     <div class="p-3 m-0 border-0">
