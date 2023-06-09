3628ac6f318424fa8c7fb97f299986179ce9f3d9
Faza Denandra <112379648+fzdeltaa@users.noreply.github.com>
2023-06-09 15:15:10 +0700

    rr

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,8 @@
 <?php
-    $hostname   = "localhost"; //bawaan
-    $username   = "root"; //bawaan
+    $hostname   = "localhost:3306"; //bawaan
+    $username   = "percet16@localhost"; //bawaan
     $password   = ""; //kosong
-    $database   = "project_akhir"; //nama database yang akan dikoneksikan
+    $database   = "percet16_wp712"; //nama database yang akan dikoneksikan
 
     $connect    = new mysqli($hostname, $username, $password, $database); //query koneksi
 
