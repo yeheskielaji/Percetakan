7f50b09d9d454874638b3989255a85b697abe970
Faza Denandra <112379648+fzdeltaa@users.noreply.github.com>
2023-06-11 00:50:47 +0700

    mumumuri

--- register_proses.php
+++ register_proses.php
@@ -7,17 +7,18 @@
 	$telp	  = $_POST['konfirmasi'];
 	$level	  = "";
 
-	// if($password =! $konfirmasi){
-	// 	header("location:login.php?message=password tidak sama");
-	// }
-
-	$sql = "INSERT INTO user VALUES('$username','$password','$level')";
-
-	$query	= mysqli_query($connect, $sql) or die(mysqli_error($connect));
-
-	if($query) {
-		header("location:login.php?message=daftar_berhasil");
+	if($password !== $telp){
+		header("location:register.php?message=beda");
 	} else {
-		header("location:login.php?message=failed");
+		$sql = "INSERT INTO user VALUES('$username','$password','$level')";
+
+		$query	= mysqli_query($connect, $sql) or die(mysqli_error($connect));
+
+		if($query) {
+			header("location:login.php?message=daftar_berhasil");
+		} else {
+			header("location:login.php?message=failed");
+		}
 	}
-?>
+
+	?>
