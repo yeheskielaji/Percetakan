6fa24512512c9d8d94cbab213f2dda3b37ac3fa1
yeheskielaji <yeheskiel.aji@gmail.com>
2023-06-11 00:12:08 +0700

    Update index_admin_invoice.php

--- index_admin_invoice.php
+++ index_admin_invoice.php
@@ -22,7 +22,7 @@ $query    = mysqli_query($connect, $sql);
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
-    <title>Toko Medical</title>
+    <title>PercetakanKU</title>
     <link rel="stylesheet" href="style/style.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
@@ -31,22 +31,26 @@ $query    = mysqli_query($connect, $sql);
     <div class="container-fluid  bg-light">
         <div class="row align-items-start">
             <div class="col-md-2 sidebar bg-white">
-                <div>
+                <div style="list-style-type:none;">
                     <img src="asset/logo cetak.png" alt="">
-                    <a href="index_admin.php">
-                        <h3>Dashboard</h3>
-                    </a>
-                    <a href="">
-                        <h3>Invoice</h3>
-                    </a>
-                    <a href="">
-                        <h3>pending</h3>
-                    </a>
-                </div>
-                <div class="fixed-bottom text-center text-md-start justify-content-between" style="padding-bottom: 30px;">
-                    <div class="mb-3 mb-md-0" style="color:grey;">
-                        <a href="">logout<i class="bi bi-box-arrow-right"></i></a>
-                    </div>
+                    <li class="nav-item active d-flex gap-1">
+                        <img src="asset/dasboard.png" alt="">
+                        <a class="nav-link" href="index_admin.php">
+                        <i class="fas fa-fw fa-tachometer-alt"></i>
+                        <span>Dashboard</span></a>
+                    </li>
+                    <li class="nav-item active d-flex gap-1">
+                        <img src="asset/invoice.png" alt="">
+                        <a class="nav-link" href="index_admin_invoice.php">
+                        <i class="fas fa-fw fa-tachometer-alt"></i>
+                        <span>Invoice</span></a>
+                    </li>
+                    <li class="nav-item active d-flex gap-1 fixed-bottom mb-5 pl-2">
+                        <img src="asset/logout.png" alt="">
+                        <a class="nav-link" href="logout.php">
+                        <i class="fas fa-fw fa-tachometer-alt"></i>
+                        <span>Logout</span></a>
+                    </li>
                 </div>
             </div>
 
