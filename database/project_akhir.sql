1a1f303fca25dadfcf28b8c77c91f1ee7a06ec0a
yeheskielaji <yeheskiel.aji@gmail.com>
2023-06-10 15:04:16 +0700

    Update project_akhir.sql

--- database/project_akhir.sql
+++ database/project_akhir.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 28, 2022 at 02:19 AM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.0.13
+-- Generation Time: Jun 10, 2023 at 10:03 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,46 +24,45 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `keranjang`
+-- Table structure for table `desainer`
 --
 
-CREATE TABLE `keranjang` (
+CREATE TABLE `desainer` (
+  `desainerid` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `desainer`
+--
+
+INSERT INTO `desainer` (`desainerid`, `username`) VALUES
+(1, 'desain');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pesanan`
+--
+
+CREATE TABLE `pesanan` (
   `keranjangid` int(11) NOT NULL,
   `username` varchar(20) NOT NULL,
   `total_harga` int(10) NOT NULL,
   `productid` int(11) NOT NULL,
   `quantity` int(3) NOT NULL,
-  `catatanorder` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `nego` int(10) NOT NULL,
+  `file` text NOT NULL,
+  `desainerid` int(11) NOT NULL,
+  `status` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `keranjang`
+-- Dumping data for table `pesanan`
 --
 
-INSERT INTO `keranjang` (`keranjangid`, `username`, `total_harga`, `productid`, `quantity`, `catatanorder`) VALUES
-(1, 'andra', 8000000, 1, 2, 'bang');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `orderstatus`
---
-
-CREATE TABLE `orderstatus` (
-  `orderstatusid` int(11) NOT NULL,
-  `waktu` datetime NOT NULL,
-  `username` varchar(20) NOT NULL,
-  `total_harga` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `orderstatus`
---
-
-INSERT INTO `orderstatus` (`orderstatusid`, `waktu`, `username`, `total_harga`) VALUES
-(1, '2022-11-26 16:56:18', 'user', 12000000),
-(2, '2022-11-26 17:17:49', 'user', 6000000),
-(3, '2022-11-26 17:27:33', 'user', 110000000);
+INSERT INTO `pesanan` (`keranjangid`, `username`, `total_harga`, `productid`, `quantity`, `nego`, `file`, `desainerid`, `status`) VALUES
+(1, 'andra', 8000000, 1, 2, 0, 'bang', 0, '');
 
 -- --------------------------------------------------------
 
@@ -75,35 +74,34 @@ CREATE TABLE `product` (
   `productid` int(11) NOT NULL,
   `name` varchar(200) NOT NULL,
   `price` int(10) NOT NULL,
-  `foto` text NOT NULL,
-  `penjelasan` varchar(105) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `foto` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `product`
 --
 
-INSERT INTO `product` (`productid`, `name`, `price`, `foto`, `penjelasan`) VALUES
-(1, 'Auto Clave', 4000000, 'autoclave.jpg', 'Autoclave adalah salah satu alat laboratorium yang digunakan untuk mensterilkan alat-alat laboratorium se'),
-(2, 'Automated Defibrillator', 2000000, 'automateddefibrillator.jpg', 'AED sendiri merupakan sebuah alat medis yang dapat menganalisis irama jantung '),
-(11, 'Face Mask', 50000, 'facemask.png', 'Alat medis yang menyediakan metode untuk mentransfer gas pernafasan oksigen'),
-(14, 'Blood Test Kit', 50000, 'blood.jpg', 'Untuk mengambil darah pada jari untuk pemeriksaan di laboratorium atau alat test darah.'),
-(15, 'Bed Screen', 1750000, 'BedScreen.jpg', 'Bed Screen atau skerem merupakan pembatas ruangan yang digunakan diruang rawat inap rumah sakit'),
-(16, 'Co Analyzer', 500000, 'CoAnalyzer.jpg', 'Untuk membantu setiap orang yang ingin mengetahui seberapa banyak tingkat CO ada di tubuh mereka'),
-(17, 'Hot Water Bag', 25000, 'hotwaterbag.jpg', 'Untuk tujuan memberikan kehangatan pada otot.'),
-(18, 'Infusion Pump', 27500000, 'infusionpump.png', 'Pompa infus adalah alat medis yang digunakan untuk mengalirkan cairan ke dalam tubuh pasien secara terken'),
-(19, 'Inhaler', 20000, 'inhaler.jpg', 'Inhaler merupakan obat hirup yang mengantarkan obat ke paru-paru untuk meredakan gejala asma.'),
-(20, 'Kursi Roda', 1500000, 'kursiroda.jpg', 'Kursi roda digunakan oleh orang yang sulit atau tidak mungkin berjalan karena sakit'),
-(21, 'Masker Medis', 20000, 'maskermedis.jpg', 'Masker medis adalah produk kesehatan untuk menutupi hidung dan mulut pemakainya'),
-(22, 'Medicine Trolley', 12000000, 'medicinetrolley.jpg', 'Lemari dorong  yang bertujuan untuk menyimpan dan mengangkut obat-obatan'),
-(23, 'Meja Operasi', 150000000, 'mejaoperasi.jpg', 'meja tempat pasien berbaring selama prosedur pembedahan'),
-(24, 'P3K', 150000, 'p3k.jpg', 'Kotak P3K berperan penting untuk kebutuhan medis darurat'),
-(25, 'Pulse Oxymeter', 0, 'pulseoxymeter.jpg', '80000'),
-(26, 'Scoop Stertcher', 4000000, 'ScoopStertcher.jpg', 'Perangkat yang digunakan khusus untuk memindahkan orang yang terluka'),
-(27, 'Stetoskop', 8000000, 'stetoskop.jpg', 'Digunakan untuk mengirimkan suara bervolume rendah seperti detak jantung ke telinga'),
-(28, 'Termometer', 25000, 'termometer.jpg', 'Perangkat yang digunakan untuk mengukur suhu tubuh manusia atau hewan'),
-(29, 'X-Ray', 200000000, 'xray.png', 'Digunakan untuk menelisik kondisi tulang dan sendi'),
-(30, 'Alcohol Swabs', 30000, 'AlcoholSwabs.jpeg', 'merupakan kapas yang dilapisi dengan alkohol murni, steril dan dikemas secara individual');
+INSERT INTO `product` (`productid`, `name`, `price`, `foto`) VALUES
+(1, 'Auto Clave', 4000000, 'autoclave.jpg'),
+(2, 'Automated Defibrillator', 2000000, 'automateddefibrillator.jpg'),
+(11, 'Face Mask', 50000, 'facemask.png'),
+(14, 'Blood Test Kit', 50000, 'blood.jpg'),
+(15, 'Bed Screen', 1750000, 'BedScreen.jpg'),
+(16, 'Co Analyzer', 500000, 'CoAnalyzer.jpg'),
+(17, 'Hot Water Bag', 25000, 'hotwaterbag.jpg'),
+(18, 'Infusion Pump', 27500000, 'infusionpump.png'),
+(19, 'Inhaler', 20000, 'inhaler.jpg'),
+(20, 'Kursi Roda', 1500000, 'kursiroda.jpg'),
+(21, 'Masker Medis', 20000, 'maskermedis.jpg'),
+(22, 'Medicine Trolley', 12000000, 'medicinetrolley.jpg'),
+(23, 'Meja Operasi', 150000000, 'mejaoperasi.jpg'),
+(24, 'P3K', 150000, 'p3k.jpg'),
+(25, 'Pulse Oxymeter', 0, 'pulseoxymeter.jpg'),
+(26, 'Scoop Stertcher', 4000000, 'ScoopStertcher.jpg'),
+(27, 'Stetoskop', 8000000, 'stetoskop.jpg'),
+(28, 'Termometer', 25000, 'termometer.jpg'),
+(29, 'X-Ray', 200000000, 'xray.png'),
+(30, 'Alcohol Swabs', 30000, 'AlcoholSwabs.jpeg');
 
 -- --------------------------------------------------------
 
@@ -114,40 +112,40 @@ INSERT INTO `product` (`productid`, `name`, `price`, `foto`, `penjelasan`) VALUE
 CREATE TABLE `user` (
   `username` varchar(20) NOT NULL,
   `password` varchar(20) NOT NULL,
-  `telp` int(20) NOT NULL,
-  `alamat` text NOT NULL,
   `level` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`username`, `password`, `telp`, `alamat`, `level`) VALUES
-('adit', 'adit', 2147483647, 'sfdgsfgadfgdfgadfgafdgafg', ''),
-('admin', 'admin', 812345678, 'wates, kulon progo', 'admin'),
-('aji', 'aji', 2147483647, 'assadadsasda', 'admin'),
-('andra', '123', 123, '123', ''),
-('user', 'user', 82131414, 'jepara, jawa tengah', '');
+INSERT INTO `user` (`username`, `password`, `level`) VALUES
+('adit', 'adit', ''),
+('admin', 'admin', 'admin'),
+('aji', 'aji', 'admin'),
+('andra', '123', ''),
+('desain', 'desain', 'desain'),
+('user', 'user', '');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `keranjang`
+-- Indexes for table `desainer`
 --
-ALTER TABLE `keranjang`
-  ADD PRIMARY KEY (`keranjangid`),
-  ADD KEY `username` (`username`),
-  ADD KEY `productid` (`productid`);
+ALTER TABLE `desainer`
+  ADD PRIMARY KEY (`desainerid`),
+  ADD KEY `username` (`username`);
 
 --
--- Indexes for table `orderstatus`
+-- Indexes for table `pesanan`
 --
-ALTER TABLE `orderstatus`
-  ADD PRIMARY KEY (`orderstatusid`),
-  ADD KEY `username` (`username`);
+ALTER TABLE `pesanan`
+  ADD PRIMARY KEY (`keranjangid`),
+  ADD KEY `username` (`username`),
+  ADD KEY `productid` (`productid`),
+  ADD KEY `desainerid` (`desainerid`);
 
 --
 -- Indexes for table `product`
@@ -166,16 +164,16 @@ ALTER TABLE `user`
 --
 
 --
--- AUTO_INCREMENT for table `keranjang`
+-- AUTO_INCREMENT for table `desainer`
 --
-ALTER TABLE `keranjang`
-  MODIFY `keranjangid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+ALTER TABLE `desainer`
+  MODIFY `desainerid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT for table `orderstatus`
+-- AUTO_INCREMENT for table `pesanan`
 --
-ALTER TABLE `orderstatus`
-  MODIFY `orderstatusid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `pesanan`
+  MODIFY `keranjangid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `product`
@@ -188,17 +186,18 @@ ALTER TABLE `product`
 --
 
 --
--- Constraints for table `keranjang`
+-- Constraints for table `desainer`
 --
-ALTER TABLE `keranjang`
-  ADD CONSTRAINT `keranjang_ibfk_2` FOREIGN KEY (`username`) REFERENCES `user` (`username`),
-  ADD CONSTRAINT `keranjang_ibfk_3` FOREIGN KEY (`productid`) REFERENCES `product` (`productid`);
+ALTER TABLE `desainer`
+  ADD CONSTRAINT `desainer_ibfk_1` FOREIGN KEY (`username`) REFERENCES `user` (`username`);
 
 --
--- Constraints for table `orderstatus`
+-- Constraints for table `pesanan`
 --
-ALTER TABLE `orderstatus`
-  ADD CONSTRAINT `orderstatus_ibfk_1` FOREIGN KEY (`username`) REFERENCES `user` (`username`);
+ALTER TABLE `pesanan`
+  ADD CONSTRAINT `pesanan_ibfk_2` FOREIGN KEY (`username`) REFERENCES `user` (`username`),
+  ADD CONSTRAINT `pesanan_ibfk_3` FOREIGN KEY (`productid`) REFERENCES `product` (`productid`),
+  ADD CONSTRAINT `pesanan_ibfk_4` FOREIGN KEY (`desainerid`) REFERENCES `desainer` (`desainerid`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
